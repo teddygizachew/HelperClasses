7e4a6ae23533a2b95ea99c8c996deaa89c65a143
teddygizachew <teddygizachew1@gmail.com>
2022-12-12 12:26:30 -0500

    Add JSON Helpers

---
+++ Helpers/JSON/beatles.json.php
@@ -0,0 +1,2 @@
+<?php die() ?>
+{"a":{"firstname":"John","lastname":"Lennon"},"b":{"firstname":"Paul","lastname":"McCartney"},"c":{"firstname":"George","lastname":"Harrison"},"d":{"firstname":"George","lastname":"Harrison"},"e":{"firstname":"Ringo","lastname":"Starr"}}
