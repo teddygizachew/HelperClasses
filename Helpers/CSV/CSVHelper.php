85e097a8013b39fcf8fe1b61bb944796452049af
teddygizachew <teddygizachew1@gmail.com>
2022-12-12 12:25:40 -0500

    Add CSV Helpers

--- Helpers/CSV/CSVHelper.php
+++ Helpers/CSV/CSVHelper.php
@@ -30,24 +30,99 @@ class CSVHelper
   static function read($filename)
   {
     $data = [];
-    // open the file
-    $fh = fopen($filename, 'r');
+    if (file_exists($filename)) {
+      // open the file
+      $fh = fopen($filename, 'r');
 
-    if ($fh === false) {
-      die('Cannot open the file ' . $filename);
+      if ($fh === false) {
+        die('Cannot open the file ' . $filename);
+      }
+
+      // read each line in CSV file at a time
+      while (($row = fgetcsv($fh)) !== false) {
+        $data[] = $row;
+      }
+
+      // close the file
+      fclose($fh);
     }
-
-    // read each line in CSV file at a time
-    while (($row = fgetcsv($fh)) !== false) {
-      $data[] = $row;
-    }
-
-    // close the file
-    fclose($fh);
     return $data;
   }
 
-  static function modify($filname) {
-    
+  static function modify($filename, $index, $data)
+  {
+    if (file_exists($filename)) {
+      $fh = fopen($filename, 'r');
+
+      $line_array = array();
+      while ($line = fgets($fh)) {
+        array_push($line_array, (trim($line)));
+      }
+      fclose($fh);
+
+      $line_counter = 0;
+      $new_file_content = '';
+      $fh = fopen($filename, 'r');
+      while ($line = fgets($fh)) {
+        if ($line_counter == $index) {
+          if ($line_array[$line_counter] != $data) {
+            $element = implode(",", $data);
+            $new_file_content .= $element . PHP_EOL;
+          }
+        } else {
+          $new_file_content .= $line;
+        }
+        $line_counter++;
+      }
+      fclose($fh);
+
+      file_put_contents($filename, $new_file_content);
+    } else {
+      echo '404: File not found!';
+    }
+  }
+
+  static function delete($filename, $index = null, $clear = false)
+  {
+    if ($clear == true) {
+      file_put_contents($filename, '');
+    } else {
+      $line_counter = 0;
+      $new_file_content = '';
+      $fh = fopen($filename, 'r');
+      while ($line = fgets($fh)) {
+        if ($line_counter == $index) {
+          $new_file_content .= '';
+        } else {
+          $new_file_content .= $line;
+        }
+        $line_counter++;
+      }
+      fclose($fh);
+
+      file_put_contents($filename, $new_file_content);
+      echo 'You\'ve successfully deleted the quote';
+    }
+    return null;
+  }
+
+  static function getUser($filename, $data)
+  {
+    if (file_exists($filename)) {
+      $fh = fopen($filename, 'r');
+
+      // read each line in CSV file at a time
+      while (($row = fgetcsv($fh)) !== false) {
+        $line = explode(";", $row[0]);
+        if (trim($line[0]) == $data) {
+          return true;
+        }
+      }
+
+      return False;
+      fclose($fh);
+    } else {
+      echo "File Does Not Exist";
+    }
   }
 }
--- Helpers/CSV/beatles.csv.php
+++ Helpers/CSV/beatles.csv.php
@@ -1,6 +1,5 @@
-Symbol,Company,Price
+John,Lennon,1940-10-09
 GOOG,"Google Inc.",800
 AAPL,"Apple Inc.",500
 AMZN,"Amazon.com Inc.",250
 YHOO,"Yahoo! Inc.",250
-FB,"Facebook, Inc.",30
